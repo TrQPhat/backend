77ccf0d26bae5ef689ff122063a101915d1201f5
Phat Truong Quang <tqphat31082k3@gmail.com>
2025-04-04 01:54:44 +0700

    update

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://backend-b9gt.onrender.com",
+    origin: "https://coffeeshop-chi-nine.vercel.app",
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
