66d28995f1213b68ec6470b47d880abe712adff3
Phat Truong Quang <tqphat31082k3@gmail.com>
2025-04-04 01:51:57 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-require('dotenv').config();
-const http = require('http');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const { initializeSocket } = require('./socket');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
+const http = require("http");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { initializeSocket } = require("./socket");
 
 const app = express();
 const port = process.env.PORT;
@@ -22,25 +22,27 @@ app.use((req, res, next) => {
 });
 
 // Kết nối database
-const { sequelize } = require('./src/app/models/index');
+const { sequelize } = require("./src/app/models/index");
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:3001",
-  credentials: true,
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization"
-}));
+app.use(
+  cors({
+    origin: "https://backend-b9gt.onrender.com",
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 // Import Router
-const router = require('./src/router');
-app.use('/api', router);
-app.use('/uploads', express.static(path.join(__dirname, '/src/uploads')));
+const router = require("./src/router");
+app.use("/api", router);
+app.use("/uploads", express.static(path.join(__dirname, "/src/uploads")));
 
 // Chạy server
 server.listen(port, () => {
